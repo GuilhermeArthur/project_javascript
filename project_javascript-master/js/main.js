bca806bd59685531160e3dcc15b2a7f49a54baea
GuilhermeArthur <66084614+GuilhermeArthur@users.noreply.github.com>
2020-06-06 18:00:40 -0300

    Add files via upload

---
+++ project_javascript-master/js/main.js
@@ -0,0 +1,98 @@
+function redirecionar (){
+    window.open("https://github.com/")
+}
+
+function passar() {
+    document.getElementById("passando").innerHTML = "Obrigado por passar o mouse"
+}
+function voltar() {
+    document.getElementById("passando").innerHTML = "Passe o mouse aqui"
+}
+
+
+
+
+
+
+
+/*function clique() {
+   document.getElementById("agrado").innerHTML = "Agradeço por Clicar"
+}
+*/
+
+
+
+
+/*function validaidade(idade) {
+   if (idade >= 18) {
+      validar = true 
+   } else {
+       validar= false
+   }
+   return validar;
+}
+var idade2 = 18
+var idade = prompt("Insira sua Idade");
+validaidade (idade)
+console.log (validar)
+*/
+
+
+
+
+/*function soma(n1, n2) {
+    return n1 + n2;
+}
+
+console.log(soma(n1=15, n2=30))
+*/
+
+
+/*var data= new Date();
+alert (data);
+alert(data.getDay());
+alert(data.getHours());
+*/
+
+
+
+/*var count;
+for (count = 0; count <=8; count = count+1);
+console.log(count)
+*/
+
+
+
+/*var count = 0;
+while (count < 5) {
+    console.log(count = count+1)
+}
+*/
+/*var idade= prompt ("Insira sua Idade");
+if (idade >= 18) {
+    alert ("Você é Maior de Idade");
+} else {
+    alert("Esse Conteúdo não é pra você");
+} 
+*/
+
+
+
+//var lista= [{nome:"Berinjela", cor:"roxa"}, {nome:"abacaxi", cor:"amarelado"}];
+//console.log (lista[0].nome);
+//alert(lista[0].cor)
+
+
+
+
+//var lista= ["Pêra","uva", "maçã", "salada"];
+//console.log(lista[1]);
+//console.log(lista.toString())
+//console.log(lista.join(" / "))
+
+
+
+
+
+//console.log(nome.replace("Arthur", "Santos"));
+//console.log(nome.toLocaleUpperCase())
